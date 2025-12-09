6b99c50b28646c671b272e8ae732cc1cc3610c27
NoiserZ <danielhajdok@gmail.com>
2025-12-09 19:15:11 +0100

    updated test

--- test_blackjack_game.py
+++ test_blackjack_game.py
@@ -154,7 +154,7 @@ def test_player_and_dealer_outcomes(blackjack_module, capsys):
 
     def run_logic(player_value, dealer_value, card_rank, expected_message, expected_sum):
         """
-        Added run logic
+        Komment1
         """
         player = Hand()
         player.value = player_value
